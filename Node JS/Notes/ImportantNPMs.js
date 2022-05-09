18a9781e34329115ef28fbfc3814ad2a0d4e9d74
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-05-09 14:22:22 +0530

    NPM Packages list

---
+++ Node JS/Notes/ImportantNPMs.js
@@ -0,0 +1,9 @@
+// => Important NPM Packages -
+
+// = Dev Packages -
+// 1. Nodemon - Dev Server for nodejs -> npm i Nodemon(dev dependency)
+
+// = Prod Packages -
+// 1. Data Functions - npm i data-fns
+// 2. UUID - Unique RFC ID -> npm i uuid -> const {v4:uuid} = require("uuid")
+// 3. ShortID - Unique Short ID Generator - npm i shortid
