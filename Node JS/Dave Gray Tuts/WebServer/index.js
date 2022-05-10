54baaf5faf652977cb1a440d31812a6fcb74feb6
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-05-10 13:54:38 +0530

    Basic  Dynamic Web server serving static complete

--- Node JS/Dave Gray Tuts/WebServer/index.js
+++ Node JS/Dave Gray Tuts/WebServer/index.js
@@ -5,19 +5,112 @@ const fsPromises = require("fs").promises;
 
 const logEvents = require("./logEvents");
 const EventEmitter = require("events");
-const { kill } = require("process");
+const { kill, emitWarning } = require("process");
 
 class Emitter extends EventEmitter {}
 const myEmitter = new Emitter();
 
-// myEmitter.on("log", (msg) => logEvents(msg));
+myEmitter.on("log", (msg, fileName) => logEvents(msg, fileName));
 // myEmitter.emit("log", "Log Event Emitted...");
 
 const HOSTNAME = "localhost";
 const PORT = process.env.PORT || 3500;
 
+const serveFile = async (filePath, contentType, response) => {
+	try {
+		const rawData = await fsPromises.readFile(
+			filePath,
+			!contentType.includes("image") ? "utf8" : ""
+		);
+		const data =
+			contentType === "application/json" ? JSON.parse(rawData) : rawData;
+		response.writeHead(filePath.includes("404.html") ? 404 : 200, {
+			"Content-Type": contentType,
+		});
+		response.end(
+			contentType === "application/json" ? JSON.stringify(data) : data
+		);
+	} catch (err) {
+		console.log(err);
+		myEmitter.emit("log", `${err.name}: ${err.message}`, "errorLog.txt");
+		response.statusCode = 500;
+		response.end();
+	}
+};
+
 const server = http.createServer((req, res) => {
 	console.log(req.url, req.method);
+	myEmitter.emit("log", `${req.url}\t${req.method}`, "reqLog.txt");
+
+	const extension = path.extname(req.url);
+	let contentType;
+
+	switch (extension) {
+		case ".css":
+			contentType = "text/css";
+			break;
+		case ".js":
+			contentType = "text/javascript";
+			break;
+		case ".json":
+			contentType = "application/json";
+			break;
+		case ".jpg":
+			contentType = "image/jpeg";
+			break;
+		case ".png":
+			contentType = "image/png";
+			break;
+		case ".txt":
+			contentType = "text/plain";
+			break;
+		default:
+			contentType = "text/html";
+			break;
+	}
+
+	let filePath =
+		contentType === "text/html" && req.url === "/"
+			? path.join(__dirname, "views", "index.html")
+			: contentType === "text/html" && req.url.slice(-1) === "/"
+			? path.join(__dirname, "views", req.url, "index.html")
+			: contentType === "text/html"
+			? path.join(__dirname, "views", req.url)
+			: path.join(__dirname, req.url);
+
+	// make .html extension not required in the browser url path
+	if (!extension && req.url.slice(-1) !== "/") {
+		filePath += ".html";
+	}
+
+	const fileExists = fs.existsSync(filePath);
+
+	if (fileExists) {
+		// serve the file
+		// res.end(fileExists);
+		serveFile(filePath, contentType, res);
+	} else {
+		// 404 Not Found
+		// 301 Redirect
+		// console.log(path.parse(filePath));
+		switch (path.parse(filePath).base) {
+			case "old-page.html":
+				res.writeHead(301, { Location: "/new-page.html" });
+				res.end();
+				break;
+			case "Www-page.html":
+				res.writeHead(301, { Location: "/" });
+				res.end();
+				break;
+			default:
+				serveFile(
+					path.join(__dirname, "views", "404.html"),
+					"text/html",
+					res
+				);
+				break;
+		}
+	}
 });
 
 server.listen(PORT, HOSTNAME, () => {
