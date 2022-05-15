cbedfda1b7b13d060bc58ecd229f45e2271a993d
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-05-15 13:21:52 +0530

    cors

--- Node JS/Notes/ImportantNPMs.js
+++ Node JS/Notes/ImportantNPMs.js
@@ -7,3 +7,5 @@
 // 1. Data Functions - npm i data-fns
 // 2. UUID - Unique RFC ID -> npm i uuid -> const {v4:uuid} = require("uuid")
 // 3. ShortID - Unique Short ID Generator - npm i shortid
+// 4. cors - For Cross Origin Resource Sharing
+// 5. express - expressJS framework
