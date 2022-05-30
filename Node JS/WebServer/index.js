e5b97177c83d25831562fa6b86171665e8da2834
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-05-30 19:15:05 +0530

    Multiple Request Endpoints

--- Node JS/WebServer/index.js
+++ Node JS/WebServer/index.js
@@ -1,7 +1,57 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-	res.write("Request Recieved");
+	// Responding with Plain text
+	// res.writeHead(200, {
+	// 	"Content-Type": "text/plain",
+	// });
+	// res.write("Request Completed");
+
+	// Responding with JSON
+	// res.writeHead(200, {
+	// 	"Content-Type": "application/json",
+	// });
+	// res.write(
+	// 	JSON.stringify({
+	// 		status: "OK",
+	// 		message: "Request Completed",
+	// 	})
+	// );
+
+	if (req.url === "/friends") {
+		res.writeHead(200, {
+			"Content-Type": "application/json",
+		});
+		res.write(
+			JSON.stringify({
+				friends: [
+					{
+						id: 1,
+						name: "Sir Issac Newton",
+					},
+					{
+						id: 2,
+						name: "Nikola Tesla",
+					},
+				],
+			})
+		);
+	} else if (req.url === "/messages") {
+		res.write(
+			"<html><body><h3>Hello</h3><h3>What are yours thougts on astronomy</h3></body></html>"
+		);
+	} else {
+		res.writeHead(404, {
+			"Content-Type": "application/json",
+		});
+		res.write(
+			JSON.stringify({
+				status: "Not Found",
+				message: "Something went wrong",
+			})
+		);
+	}
+
 	res.end();
 });
 
