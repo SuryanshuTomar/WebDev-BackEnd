e95528d44306e41641c804964d213a4e76c8196d
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-06-05 12:48:31 +0530

    Adding Nested Params

--- Node JS/WebServer/index.js
+++ Node JS/WebServer/index.js
@@ -18,25 +18,42 @@ const server = http.createServer((req, res) => {
 	// 	})
 	// );
 
-	if (req.url === "/friends") {
+	const friends = [
+		{
+			id: 0,
+			name: "Satyendra Nath Bose",
+		},
+		{
+			id: 1,
+			name: "Sir Issac Newton",
+		},
+		{
+			id: 2,
+			name: "Nikola Tesla",
+		},
+	];
+	console.log(req.url);
+	const params = req.url.split("/");
+	console.log(params);
+	// /friends/2 => ["", "friends", "2"]
+	if (params[1] === "friends") {
+		// res.statusCode = 200;
+		// res.setHeader("Content-Type", "application/json");
 		res.writeHead(200, {
 			"Content-Type": "application/json",
 		});
-		res.write(
-			JSON.stringify({
-				friends: [
-					{
-						id: 1,
-						name: "Sir Issac Newton",
-					},
-					{
-						id: 2,
-						name: "Nikola Tesla",
-					},
-				],
-			})
-		);
-	} else if (req.url === "/messages") {
+		if (Number(params[2]) >= friends.length) {
+			res.writeHead(404, {
+				"Content-Type": "application/json",
+			});
+			res.write(JSON.stringify({ status: 404, message: "No Friend found" }));
+		} else if (params.length === 3) {
+			const friendIndex = Number(params[2]);
+			res.write(JSON.stringify(friends[friendIndex]));
+		} else {
+			res.write(JSON.stringify(friends));
+		}
+	} else if (params[1] === "messages") {
 		res.write(
 			"<html><body><h3>Hello</h3><h3>What are yours thougts on astronomy</h3></body></html>"
 		);
