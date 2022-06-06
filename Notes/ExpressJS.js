461ecee20a7eb4044a15884275e27543632dca6c
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-06-06 10:57:03 +0530

    What is Express js

---
+++ Notes/ExpressJS.js
@@ -0,0 +1,7 @@
+// => WHAT IS EXPRESS JS ?
+
+// - Express is a node js web application framework that provides broad features for building web and mobile applications. It is used to build a single page, multipage, and hybrid web application.
+// - It's a layer built on the top of the Node js that helps manage servers and routes.
+
+// - More On -
+// https://www.simplilearn.com/tutorials/nodejs-tutorial/what-is-express-js?source=sl_frs_nav_playlist_video_clicked
