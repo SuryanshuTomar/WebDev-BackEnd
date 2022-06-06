35185acdd16e34e0c3677b9328973c7d2208ea2f
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-06-06 12:46:54 +0530

    Setting Routes

--- ExpressJS/server.js
+++ ExpressJS/server.js
@@ -6,6 +6,40 @@ const app = express();
 // PORT
 const PORT = 3500;
 
+// Data
+const friends = [
+	{
+		id: 0,
+		name: "Satyendra Nath Bose",
+	},
+	{
+		id: 1,
+		name: "Iron Man",
+	},
+];
+
+// Routes
+// app.get(pathEndpoint, HandlerFunction(request, response, next))
+app.get("/friends", (req, res) => {
+	res.json(friends);
+});
+
+// Nested Route
+app.get("/friends/:id", (req, res) => {
+	const friendsId = Number(req.params.id);
+	const friend = friends[friendsId];
+	if (friend) res.status(200).json(friend);
+	else res.status(404).send("No Friend Found");
+});
+
+app.get("/messages", (req, res) => {
+	res.send("<h1>Hello My Friend!!</h1>");
+});
+
+app.post("/messages", (req, res) => {
+	console.log("Updating Messages...");
+});
+
 // Listening to the application server
 app.listen(PORT, "localhost", () => {
 	console.log(`Listening on PORT : ${PORT}`);
