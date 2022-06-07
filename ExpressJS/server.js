9fe11c2e295e51e2d098503fc16fc324dc73627c
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-06-07 12:55:40 +0530

    Json Paror Middleware and Post Request

--- ExpressJS/server.js
+++ ExpressJS/server.js
@@ -26,6 +26,9 @@ app.use((req, res, next) => {
 	console.log(`${req.method} : ${req.url}  ${delta}ms`);
 });
 
+// For Parsing JSON Body
+app.use(express.json());
+
 // Routes -
 // app.get(pathEndpoint, HandlerFunction(request, response))
 app.get("/friends", (req, res) => {
@@ -37,11 +40,29 @@ app.get("/friends/:id", (req, res) => {
 	const friendsId = Number(req.params.id);
 	const friend = friends[friendsId];
 	if (friend) res.status(200).json(friend);
-	else res.status(404).send("No Friend Found");
+	else
+		res.status(404).json({
+			error: "No Friends Found",
+		});
 });
 
 app.get("/messages", (req, res) => {
 	res.send("<h1>Hello My Friend!!</h1>");
+});
+
+app.post("/friends", (req, res) => {
+	if (!req.body.name) {
+		return res.status(400).json({
+			error: "Missing friend name",
+		});
+	}
+
+	const newFriend = {
+		id: friends.length,
+		name: req.body.name,
+	};
+	friends.push(newFriend);
+	res.json(newFriend);
 });
 
 app.post("/messages", (req, res) => {
