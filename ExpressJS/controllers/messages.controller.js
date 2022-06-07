f013e402bd3c65198542bd1eb32328522c8909e5
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-06-08 00:01:11 +0530

    Sending File to the Browser

--- ExpressJS/controllers/messages.controller.js
+++ ExpressJS/controllers/messages.controller.js
@@ -1,5 +1,8 @@
+const path = require("path");
+
 function getMessages(req, res) {
-	res.send("<h1>Hello My Friend!!</h1>");
+	// sending file
+	res.sendFile(path.join(__dirname, "..", "public", "images", "photo.png"));
 }
 
 function postMessage(req, res) {
