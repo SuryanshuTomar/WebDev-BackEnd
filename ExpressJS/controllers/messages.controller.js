0f38be769ded817bd6aab1f57279eafed14c09f2
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-06-08 11:59:41 +0530

    Layout in HBS

--- ExpressJS/controllers/messages.controller.js
+++ ExpressJS/controllers/messages.controller.js
@@ -2,7 +2,13 @@ const path = require("path");
 
 function getMessages(req, res) {
 	// sending file
-	res.sendFile(path.join(__dirname, "..", "public", "images", "photo.png"));
+	// res.sendFile(path.join(__dirname, "..", "public", "images", "photo.png"));
+
+	// rendering messages hbs file
+	res.render("messages", {
+		title: "Message to my friend",
+		friend: "Elon Musk",
+	});
 }
 
 function postMessage(req, res) {
