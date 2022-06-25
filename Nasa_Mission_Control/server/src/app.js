3ce05057af99763e59c0b7bbc95e0fd9e83d94e7
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-06-25 18:55:07 +0530

    added cors middleware

--- Nasa_Mission_Control/server/src/app.js
+++ Nasa_Mission_Control/server/src/app.js
@@ -1,6 +1,8 @@
 // Imports
 const express = require("express");
+const cors = require("cors");
 
+// Router Imports
 const planetsRouter = require("./routes/planets/planets.router");
 
 // Creating express server app
@@ -8,6 +10,13 @@ const app = express();
 
 // JSON Parser Middleware
 app.use(express.json());
+
+// CORS Middleware
+app.use(
+	cors({
+		origin: "http://localhost:3000",
+	})
+);
 
 // Routers Middlewares
 app.use(planetsRouter);
