1299041722170d8930fd6024250c1ebaf1ac4adf
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-06-27 09:30:01 +0530

    added concurrently and killport

--- Notes/ImportantNPMs.js
+++ Notes/ImportantNPMs.js
@@ -1,16 +1,18 @@
 // => Important NPM Packages -
 
 // = Dev Packages -
-// 1. Nodemon - Dev Server for nodejs -> npm i Nodemon(dev dependency)
-// 2. faker-js - Used to create dummy data for our project - npm install @faker-js/faker --save-dev
+// 1. nodemon - Dev Server for nodejs -> npm i Nodemon(dev dependency)
+// 2. faker-js - Used to create dummy data for our project -> npm install @faker-js/faker --save-dev
+// 3. concurrently - It is useful for running multiple commands in the shell or in package.json.
+// 4. kill-port - It is used to kill the running port -> kill-port [PORT_NO] (kill_port 3000)
 
 // = Prod Packages -
-// 1. Data Functions - npm i data-fns
+// 1. Data Functions -> npm i data-fns
 // 2. UUID - Unique RFC ID -> npm i uuid -> const {v4:uuid} = require("uuid")
-// 3. ShortID - Unique Short ID Generator - npm i shortid
+// 3. ShortID - Unique Short ID Generator -> npm i shortid
 // 4. cors - For Cross Origin Resource Sharing
 // 5. express - expressJS framework
 
 // = React Packages -
-// 1. React Router - npm install react-router-dom
-// 2. Styles Components - npm install styled-components
+// 1. React Router -> npm install react-router-dom
+// 2. Styles Components -> npm install styled-components
