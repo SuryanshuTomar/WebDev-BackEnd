181731d63ef86fdb98b557db67ad1d751044fdfd
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-09-02 19:59:54 +0530

    serving all kind of static files using html server

--- Node JS/WebServer/index.js
+++ Node JS/WebServer/index.js
@@ -2,7 +2,10 @@ const http = require("http");
 const { readFileSync } = require("fs");
 
 // get all files
-const homePage = readFileSync("./index.html");
+const homePage = readFileSync("./navbar/index.html");
+const homePageLogo = readFileSync("./navbar/logo.svg");
+const homePageLogic = readFileSync("./navbar/browser-app.js");
+const homePageStyle = readFileSync("./navbar/styles.css");
 
 const server = http.createServer((req, res) => {
 	const url = req.url;
@@ -17,9 +20,31 @@ const server = http.createServer((req, res) => {
 		// serving the html files instead of writing the html page here.
 		res.write(homePage);
 	}
+
+	// styles
+	else if (url === "/styles.css") {
+		res.writeHead(200, { "Content-Type": "text/css" });
+		res.write(homePageStyle);
+	}
+
+	// image/logo
+	else if (url === "/logo.svg") {
+		res.writeHead(200, { "Content-Type": "image/svg+xml" });
+		res.write(homePageLogo);
+	}
+
+	// logic
+	else if (url === "/browser-app.js") {
+		res.writeHead(200, { "Content-Type": "text/javascript" });
+		res.write(homePageLogic);
+	}
+
 	// About Page
 	else if (url === "/about") {
+		res.writeHead(200, { "Content-Type": "text/html" });
+		res.write("<h1>About Page</h1>");
 	}
+
 	// All other pages
 	else {
 		res.writeHead(404, { "Content-Type": "text/html" });
