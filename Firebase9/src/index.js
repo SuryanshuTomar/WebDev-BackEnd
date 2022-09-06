83c4e341908982f9e802ab4c040825afffa84acc
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-09-07 00:11:49 +0530

    initialize firestore, get collectionRef & getDocs

--- Firebase9/src/index.js
+++ Firebase9/src/index.js
@@ -1,5 +1,6 @@
 // import initializeApp function from firebase/app to initailize our firebase app instanceof.
 import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
 
 // Firebase config settings of our app from the firebase console.
 const firebaseConfig = {
@@ -11,4 +12,22 @@ const firebaseConfig = {
 	appId: "1:186833937148:web:45e65fa9052264b41fed79",
 };
 
+// Initialize Firebase App
 initializeApp(firebaseConfig);
+
+// Init Services
+const db = getFirestore();
+
+// Collection Ref
+const colRef = collection(db, "books");
+
+// Get collection Data
+getDocs(colRef)
+	.then((snapshot) => {
+		let books = [];
+		snapshot.docs.forEach((doc) => {
+			books.push({ ...doc.data(), id: doc.id });
+		});
+		console.log(books);
+	})
+	.catch((error) => console.error(error));
