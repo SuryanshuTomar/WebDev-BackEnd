e4aaeaef04799d413844b62920f54e67adfd571e
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-09-06 18:50:08 +0530

    app.js

---
+++ Node JS/Express/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+// Setup static middleware
+app.use(express.static("./public"));
+
+app.get("/", (req, res) => {
+	res.status(200).sendFile(path.resolve(__dirname, "./navbar/index.html"));
+});
+
+app.get("/about", (req, res) => {
+	res.status(200).send("<h1>About Page</h1>");
+});
+
+// for handling all other routes that are not defined by our server application
+app.all("*", (req, res) => {
+	res.status(404).send("<h1>404 | Resource Not Found !!</h1>");
+});
+
+// => Listening to port
+const PORT = 3500;
+const URL = "localhost";
+app.listen(PORT, URL, () => {
+	console.log("Listening on PORT : ", PORT);
+});
