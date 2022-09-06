2499cf0c413f78bda6b22f8b04d39cecb1c5ef0c
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-09-06 19:02:07 +0530

    API vs SSR

--- Node JS/Express/ReferenceNotes.js
+++ Node JS/Express/ReferenceNotes.js
@@ -17,17 +17,23 @@
 // app.use()
 // app.listen()
 
-
 // => Setup static middleware -
 // Using app.use() middle and static method from express to server the public folder which contains all static files of our server app. (Static files are the files which we dont need to be change by our server app)
 // app.use(express.static("./public"));
 
-// => For sending file as a response from the server - 
+// => For sending file as a response from the server -
 // app.get("/", (req, res) => {
 // 	res.status(200).sendFile(path.resolve(__dirname, "./navbar/index.html"));
+// Note: No Need to even setup this resource path if we just put our index.html file as well in the public folder in which we are serving all the static files using the express.static() method.
 // });
 
+// => Better method of sending files from the server -
+// 1. Adding to Static Assets. (Line - 22)
+// 2. Server Side Rendering using Template Engines
 
+// -> So, we can server the data from the server to the client using one of the two methods mentioned below -
+//    1. Serving the APIs using JSON(Javascript Object Notation)(API)
+//    2. Serving the whole pages using Template Engines()(SSR)
 
 // => For handling all other routes that are not defined by our server application
 // app.all("*", (req, res) => {
