e17f175933266d63092ea13cf430500b39e102f4
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-09-06 23:44:10 +0530

    added config setting for firebase

---
+++ Firebase9/src/index.js
@@ -0,0 +1,9 @@
+// Firebase config settings of our app from the firebase console.
+const firebaseConfig = {
+	apiKey: "AIzaSyDerrz9KTo65qL5uDGDzVcDrkawNggMerw",
+	authDomain: "learn-firebase-9-82f96.firebaseapp.com",
+	projectId: "learn-firebase-9-82f96",
+	storageBucket: "learn-firebase-9-82f96.appspot.com",
+	messagingSenderId: "186833937148",
+	appId: "1:186833937148:web:45e65fa9052264b41fed79",
+};
