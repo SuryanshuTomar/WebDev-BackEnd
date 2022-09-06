e06255a21c7718d991e496d971a546d8d141968e
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-09-07 00:39:31 +0530

    added logic for adding and deleting books

--- Firebase9/src/index.js
+++ Firebase9/src/index.js
@@ -1,8 +1,16 @@
-// import initializeApp function from firebase/app to initailize our firebase app instanceof.
+// -> import initializeApp function from firebase/app to initailize our firebase app instanceof.
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+import {
+	getFirestore,
+	collection,
+	getDocs,
+	addDoc,
+	updateDoc,
+	doc,
+	deleteDoc,
+} from "firebase/firestore";
 
-// Firebase config settings of our app from the firebase console.
+// -> Firebase config settings of our app from the firebase console.
 const firebaseConfig = {
 	apiKey: "AIzaSyDerrz9KTo65qL5uDGDzVcDrkawNggMerw",
 	authDomain: "learn-firebase-9-82f96.firebaseapp.com",
@@ -12,16 +20,17 @@ const firebaseConfig = {
 	appId: "1:186833937148:web:45e65fa9052264b41fed79",
 };
 
-// Initialize Firebase App
+// -> Initialize Firebase App
 initializeApp(firebaseConfig);
 
-// Init Services
+// -> Init Services
 const db = getFirestore();
 
-// Collection Ref
-const colRef = collection(db, "books");
+// -> Collection Ref
+const colRef = collection(db, "books"); // collection(FirebaseInstance(database), collectionName)
 
-// Get collection Data
+// -> Get collection Data
+// getDocs(collectionReferece) => It will return a promise
 getDocs(colRef)
 	.then((snapshot) => {
 		let books = [];
@@ -31,3 +40,32 @@ getDocs(colRef)
 		console.log(books);
 	})
 	.catch((error) => console.error(error));
+
+// -> Adding documents
+// addDoc(collectionReference, DataObject);
+const addBookForm = document.querySelector(".add");
+addBookForm.addEventListener("submit", (event) => {
+	event.preventDefault();
+
+	addDoc(colRef, {
+		title: addBookForm.title.value,
+		author: addBookForm.author.value,
+	})
+		.then(() => addBookForm.reset())
+		.catch((err) => console.log(err));
+});
+
+// -> Deleting documents
+// - First, get the reference of the document you want to delete using doc()
+// - Second, call the deleteDoc() method
+// doc(FirebaseInstance(database), collectionName)
+// deleteDoc(docReference);
+const deleteBookForm = document.querySelector(".delete");
+deleteBookForm.addEventListener("submit", (event) => {
+	event.preventDefault();
+
+	const docRef = doc(db, "books", deleteBookForm.id.value);
+	deleteDoc(docRef)
+		.then(() => deleteBookForm.reset())
+		.catch(() => console.log(doc));
+});
