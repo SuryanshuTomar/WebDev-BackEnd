b7e5ac304cb1905b6c7e7bba64cc9b0050425896
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-09-07 17:53:41 +0530

    Route and query parameters

--- Node JS/Express/app.js
+++ Node JS/Express/app.js
@@ -1,13 +1,64 @@
 const express = require("express");
-const path = require("path");
 const app = express();
 
-// Setup static middleware
-app.use(express.static("./public"));
+const { products } = require("./data/data");
 
-// app.get("/", (req, res) => {
-// 	res.status(200).sendFile(path.resolve(__dirname, "./navbar/index.html"));
-// });
+// Setup static middleware
+// app.use(express.static("./public"));
+
+app.get("/", (req, res) => {
+	res.status(200).send(
+		"<h1>Home Page</h1> <br/><a href='/api/products'>Products</a>"
+	);
+});
+
+app.get("/api/products", (req, res) => {
+	const newProducts = products.map((prod) => {
+		const { id, name, image } = prod;
+		return { id, name, image };
+	});
+	res.status(200).json(newProducts);
+});
+
+// Using Route Parameter
+// here /:prodId is placeholder
+app.get("/api/products/:prodId", (req, res) => {
+	const productId = +req.params.prodId;
+	const singleProd = products.find((prod) => prod.id === productId);
+	if (!singleProd) {
+		return res.status(404).json({
+			status: false,
+			message: "Product does not exists",
+		});
+	}
+
+	res.status(200).json(singleProd);
+});
+
+// Using Query String Parameter
+app.get("/api/v1/query", (req, res) => {
+	const { search, limit } = req.query;
+	let sortedProducts = [...products];
+
+	if (search) {
+		sortedProducts = sortedProducts.filter((prod) =>
+			prod.name.startsWith(search)
+		);
+	}
+	if (limit) {
+		sortedProducts = sortedProducts.slice(0, Number(limit));
+	}
+
+	if (sortedProducts.length === 0) {
+		res.status(200).json({
+			success: true,
+			message: "No Products matched your search query",
+			data: [],
+		});
+	} else {
+		res.status(200).json(sortedProducts);
+	}
+});
 
 app.get("/about", (req, res) => {
 	res.status(200).send("<h1>About Page</h1>");
