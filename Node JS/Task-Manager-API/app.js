15f938441fef3fa8626f3cc53bae6d73074a3ce5
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-09-09 18:10:07 +0530

    routes and controller setup complete

--- Node JS/Task-Manager-API/app.js
+++ Node JS/Task-Manager-API/app.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const morgan = require("morgan");
 
-const { tasksRouter } = require("./routes/tasks.routes");
+const tasksRouter = require("./routes/tasks.routes");
 
 // Express app -
 const app = express();
--- Node JS/Task-Manager-API/controllers/tasks.controller.js
+++ Node JS/Task-Manager-API/controllers/tasks.controller.js
@@ -1,11 +1,21 @@
-const getAllTasks = (req, res) => {};
+const getAllTasks = (req, res) => {
+	res.send("Get All Items");
+};
 
-const createTask = (req, res) => {};
+const createTask = (req, res) => {
+	res.send("Create Task");
+};
 
-const getTask = (req, res) => {};
+const getTask = (req, res) => {
+	res.send("Get Item");
+};
 
-const updateTask = (req, res) => {};
+const updateTask = (req, res) => {
+	res.send("Update task");
+};
 
-const deleteTask = (req, res) => {};
+const deleteTask = (req, res) => {
+	res.send("Delete task");
+};
 
 module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
