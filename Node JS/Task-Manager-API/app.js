730b53255ce04077bd1d81ce4ab5ae9392e2d984
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-09-09 17:52:27 +0530

    server setup

--- Node JS/Task-Manager-API/app.js
+++ Node JS/Task-Manager-API/app.js
@@ -1,1 +1,28 @@
-console.log('Task Manager App')
+// Imports -
+const express = require("express");
+const morgan = require("morgan");
+
+const { tasksRouter } = require("./routes/tasks.routes");
+
+// Express app -
+const app = express();
+
+// Middlewares -
+// body parser
+app.use(express.json());
+// logger
+app.use(morgan("tiny"));
+
+// Routes -
+app.use("/api/v1/tasks", tasksRouter);
+
+app.get("/", (req, res) => {
+	res.status(200).send("Task Manager APP");
+});
+
+// Listerning to server -
+const PORT = 3500;
+const HOST = "localhost";
+app.listen(PORT, HOST, () => {
+	console.log(`Listening on Port : ${PORT}`);
+});
