3351af9d867c7fe4c0836dca8cfac5384921a1fc
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-09-10 18:44:46 +0530

    added controller logic for CRUD operations of API

--- Node JS/Task-Manager-API/controllers/tasks.controller.js
+++ Node JS/Task-Manager-API/controllers/tasks.controller.js
@@ -1,24 +1,113 @@
+const { findOneAndUpdate, schema } = require("../models/tasks.models");
 const Task = require("../models/tasks.models");
 
-const getAllTasks = (req, res) => {
-	res.send("Get All Items");
+const getAllTasks = async (req, res) => {
+	try {
+		const tasks = await Task.find({});
+		res.status(200).json({
+			success: true,
+			data: tasks,
+		});
+	} catch (err) {
+		res.status(500).json({
+			success: false,
+			msg: err.message,
+		});
+	}
 };
 
 const createTask = async (req, res) => {
-	const task = await Task.create(req.body);
-	res.status(201).json(task);
+	try {
+		const task = await Task.create(req.body);
+		res.status(201).json({
+			success: true,
+			data: task,
+		});
+	} catch (err) {
+		res.status(500).json({
+			success: false,
+			msg: err.message,
+		});
+	}
 };
 
-const getTask = (req, res) => {
-	res.send("Get Item");
+const getTask = async (req, res) => {
+	try {
+		const { id: taskId } = req.params;
+		const task = await Task.findOne({ _id: taskId });
+
+		if (!task) {
+			return res.status(404).json({
+				success: false,
+				msg: `No task with id : ${taskId} found `,
+			});
+		}
+
+		res.status(200).json({
+			success: true,
+			data: task,
+		});
+	} catch (err) {
+		res.status(500).json({
+			success: false,
+			msg: err.message,
+		});
+	}
 };
 
-const updateTask = (req, res) => {
-	res.send("Update task");
+const updateTask = async (req, res) => {
+	try {
+		const { id: taskId } = req.params;
+		const updatedTask = await Task.findOneAndUpdate(
+			{ _id: taskId },
+			req.body,
+			{ new: true, runValidators: true }
+			// The third paramter whic is the options object is mandatory, if we don't use this options object then :
+			// 1. The findOneAndUpdate() will return the old document
+			// 2. The validation for the current model will not run which we have defined in our schema.
+		);
+
+		if (!updatedTask) {
+			return res.status(404).json({
+				success: false,
+				msg: `No task with id : ${taskId} found `,
+			});
+		}
+
+		res.status(200).json({
+			success: true,
+			data: updatedTask,
+		});
+	} catch (err) {
+		res.status(500).json({
+			success: false,
+			msg: err.message,
+		});
+	}
 };
 
-const deleteTask = (req, res) => {
-	res.send("Delete task");
+const deleteTask = async (req, res) => {
+	try {
+		const { id: taskId } = req.params;
+		const deletedTask = await Task.findOneAndDelete({ _id: taskId });
+
+		if (!deletedTask) {
+			return res.status(404).json({
+				success: false,
+				msg: `No task found with id : ${taskId}`,
+			});
+		}
+
+		res.status(200).json({
+			success: true,
+			data: deletedTask,
+		});
+	} catch (err) {
+		res.status(500).json({
+			success: false,
+			msg: err.message,
+		});
+	}
 };
 
 module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
