607bc3469c275b6b4fcdf9058403bc37df5b5f51
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-09-12 16:29:16 +0530

    logginIn , subscribe and unsubscribe

--- Firebase9/src/index.js
+++ Firebase9/src/index.js
@@ -21,6 +21,9 @@ import {
 	createUserWithEmailAndPassword,
 	signInWithPopup,
 	GoogleAuthProvider,
+	signInWithEmailAndPassword,
+	signOut,
+	onAuthStateChanged,
 } from "firebase/auth";
 
 // -> Firebase config settings of our app from the firebase console.
@@ -81,7 +84,7 @@ const colRef = collection(db, "books"); // collection(FirebaseInstance(database)
 // );
 
 const bookQueryRef = query(colRef, orderBy("createdAt"));
-onSnapshot(bookQueryRef, (snapshot) => {
+const unsubColRef = onSnapshot(bookQueryRef, (snapshot) => {
 	let books = [];
 	snapshot.docs.forEach((doc) => {
 		books.push({ ...doc.data(), id: doc.id });
@@ -130,7 +133,7 @@ const docRef = doc(db, "books", "iJz8JBjWlSUNVqjI7M68");
 // -> Real Time collection on a Single doc (setting subscription on a single doc)
 // onSnapshot(documentReference, callBackfunction)
 // If pass a documentReference instead of collectionReference, then it will set a Real Time subscription for that document.
-onSnapshot(docRef, (docSnapshot) => {
+const unsubDocRef = onSnapshot(docRef, (docSnapshot) => {
 	console.log(docSnapshot.data(), docSnapshot.id);
 });
 
@@ -148,7 +151,7 @@ updateDocForm.addEventListener("submit", (event) => {
 
 // -> Signing Users up
 // createUserWithEmailAndPassword(AuthInstance, email, password);
-// This will create a new user with the provided email and password.
+// This will create a new user with the provided email and password and also automatically signin the user.
 const signUpForm = document.querySelector(".signup");
 signUpForm.addEventListener("submit", (event) => {
 	event.preventDefault();
@@ -160,4 +163,68 @@ signUpForm.addEventListener("submit", (event) => {
 			signUpForm.reset();
 		})
 		.catch((err) => console.log(err));
+});
+
+// -> Signing In With Google Auth
+const signInWithGoogle = document.querySelector(".login-google");
+signInWithGoogle.addEventListener("click", (event) => {
+	event.preventDefault();
+	signInWithPopup(auth, provider)
+		.then((result) => {
+			// This gives you a Google Access Token. You can use it to access the Google API.
+			const credential = GoogleAuthProvider.credentialFromResult(result);
+			const token = credential.accessToken;
+			// The signed-in user info.
+			const user = result.user;
+			// ...
+
+			console.log(token, user);
+		})
+		.catch((error) => {
+			// Handle Errors here.
+			const errorCode = error.code;
+			const errorMessage = error.message;
+			// The email of the user's account used.
+			const email = error.customData.email;
+			// The AuthCredential type that was used.
+			const credential = GoogleAuthProvider.credentialFromError(error);
+			// ...
+		});
+});
+
+// -> Logout User
+const logoutBtn = document.querySelector(".logout");
+logoutBtn.addEventListener("click", () => {
+	signOut(auth)
+		// .then(() => console.log("User Signed Out..."))
+		.catch((err) => console.log(err));
+});
+
+// -> Sign In User using Email and Password
+const loginForm = document.querySelector(".login");
+loginForm.addEventListener("submit", (event) => {
+	event.preventDefault();
+	const email = loginForm.email.value;
+	const password = loginForm.password.value;
+	signInWithEmailAndPassword(auth, email, password)
+		.then((cred) => {
+			// 	console.log("User Logged in : ", cred.user);
+		})
+		.catch((err) => console.log(err));
+});
+
+// -> Subscribing to Auth instance,
+// The function below will triggered every time there is change in the Auth Instance, like user logging In and Logging Out
+const unsubAuthRef = onAuthStateChanged(auth, (user) => {
+	console.log("User Current State : ", user);
+});
+
+// -> Unsubscirbing from our subscriptions which we setup
+// All Subscription function which we have used above returns the unsubscribe function which we can later call to unsubscribe to the Subscription.
+const unsubscribeBtn = document.querySelector(".unsub");
+unsubscribeBtn.addEventListener("click", () => {
+	console.log("Unsubscribed!");
+	unsubColRef();
+	unsubDocRef();
+	unsubAuthRef();
 });
