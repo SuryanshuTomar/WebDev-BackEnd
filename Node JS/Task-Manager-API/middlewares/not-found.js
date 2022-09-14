5517bd10f1cfd727a86d056b9396d41197c29ac7
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-09-14 17:15:03 +0530

    return added

--- Node JS/Task-Manager-API/middlewares/not-found.js
+++ Node JS/Task-Manager-API/middlewares/not-found.js
@@ -1,5 +1,5 @@
 const notFound = (req, res) => {
-	res.status(404).send("<h2>Route Does not Exist !</h2>");
+	return res.status(404).send("<h2>Route Does not Exist !</h2>");
 };
 
 module.exports = notFound;
