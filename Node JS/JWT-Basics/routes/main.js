f13a9678fe440be155039de82d2486f159e9cf07
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-09-22 21:38:38 +0530

    router setup for login and dashBoard

--- Node JS/JWT-Basics/routes/main.js
+++ Node JS/JWT-Basics/routes/main.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { login, dashboard } = require("../controllers/main");
+const { login, dashBoard } = require("../controllers/main");
 
-router.route("/dashboard").get(dashboard);
+router.route("/dashboard").get(dashBoard);
 router.route("/login").post(login);
 
 module.exports = router;
