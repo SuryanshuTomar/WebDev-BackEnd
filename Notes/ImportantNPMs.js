bb43767f907b88f0a3dc601f2496945b73133371
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-02-15 00:46:09 +0530

    added charts and cryptojs

--- Notes/ImportantNPMs.js
+++ Notes/ImportantNPMs.js
@@ -5,6 +5,9 @@
 // 2. faker-js - Used to create dummy data for our project -> npm install @faker-js/faker --save-dev
 // 3. concurrently - It is useful for running multiple commands in the shell or in package.json.
 // 4. kill-port - It is used to kill the running port -> kill-port [PORT_NO] (kill_port 3000)
+// 5. json-server - It is used for creating a backend server from a json file which already has a data.
+//     CMD -> npx json-server -w(for watch) file_path -p(for port) port_number
+//     eg -> npm json-server -w data/products.json -p 3500
 
 // = Prod Packages -
 // 1. Data Functions -> npm i data-fns
@@ -15,8 +18,9 @@
 // 6. immer - for updating the complex state in redux and react easily
 // 7. redux-logger - for logging redux states
 // 8. bcrypt - for hashing passwords
-// 9. validator- for validating emails and passwords
-// 10. jsonwebtoken - for creating jwt(Json Web Tokens)
+// 9. cryptojs - more reliable library for hashing passowords
+// 10. validator- for validating emails and passwords
+// 11. jsonwebtoken - for creating jwt(Json Web Tokens)
 
 // = React Packages -
 // 1. React Router -> npm install react-router-dom
@@ -24,4 +28,5 @@
 // 3. Framer Motion - npm install framer-motion
 // 4. TanStack React Query ->
 //           npm install @tanstack/react-query and @tanstack/react-query-devtools.
-// 5. Redux Toolkit and Redux -> npm install @reduxjs/toolkit and npm install react-redux
+// 5. Redux Toolkit and Redux -> npm install @reduxjs/toolkit and npm install react-redux7
+// 6. Charts -> recharts and chartjs, Note: for React -> chartjs and react-chart-2 both needed
