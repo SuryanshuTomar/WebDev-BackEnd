108003456657ceeced68700a07aa398627648ef4
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-04-25 11:59:35 +0530

    Async and Sync File Read

--- SixPackNodeBackend/index.js
+++ SixPackNodeBackend/index.js
@@ -1,13 +1,32 @@
 import http from "http";
+import fs from "fs";
+import path from "path";
 import { gfName as girl, generateRandomPercentage } from "./features.js";
-
-const server = http.createServer((req, res) => {
-	console.log(req.url);
-	res.end("Noice");
-});
 
 console.log(girl);
 console.log(generateRandomPercentage());
+
+// Async File Read
+// console.log(process.cwd() + "\\" + path.basename("./features.js"));
+// fs.readFile(process.cwd() + "\\" + path.basename("./features.js"), () => {
+// 	console.log("File Read");
+// });
+
+// Sync File Read
+const home = fs.readFileSync("./index.html");
+
+const server = http.createServer((req, res) => {
+	console.log(req.url);
+
+	// fs.readFile(
+	// 	process.cwd() + "\\" + path.basename("./index.html"),
+	// 	(err, homeData) => {
+	// 		res.end(homeData);
+	// 	}
+	// );
+
+	res.end(home);
+});
 
 server.listen(8000, () => {
 	console.log("Server Online !");
