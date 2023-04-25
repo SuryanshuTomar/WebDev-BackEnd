678b26669d34a9bcf1146bb4623cd117b7ce65d3
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-04-25 11:38:39 +0530

    import alias

--- SixPackNodeBackend/index.js
+++ SixPackNodeBackend/index.js
@@ -1,12 +1,12 @@
 import http from "http";
-import { gfName } from "./features.js";
+import { gfName as girl } from "./features.js";
 
 const server = http.createServer((req, res) => {
 	console.log(req.url);
 	res.end("Noice");
 });
 
-console.log(gfName);
+console.log(girl);
 
 server.listen(8000, () => {
 	console.log("Server Online !");
