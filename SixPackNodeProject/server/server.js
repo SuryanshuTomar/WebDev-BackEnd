543bee8a07873429d6ebc42ac149a14856c70907
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-05-01 18:36:26 +0530

    added env variables

--- SixPackNodeProject/server/server.js
+++ SixPackNodeProject/server/server.js
@@ -1,13 +1,19 @@
 import { connectDb } from "./db/connectDb.js";
 import { app } from "./app.js";
+import dotenv from "dotenv";
+
+// configuring env file
+dotenv.config({
+	path: "./config.env",
+});
 
 // start server
 async function startServer() {
+	const PORT = process.env.PORT;
+	const HOST = process.env.HOST;
+	const URI = process.env.MONGO_URI;
 	try {
-		await connectDb("mongodb://127.0.0.1:27017");
-
-		const PORT = 8000;
-		const HOST = "localhost";
+		await connectDb(URI);
 		app.listen(PORT, HOST, () => {
 			console.log(`Server Listening on ${PORT}...`);
 		});
