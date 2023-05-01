3e9665616d4ccd648d2bcc44e147eb1bbbe93065
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-05-01 18:36:36 +0530

    users routes

--- SixPackNodeProject/server/routes/users.routes.js
+++ SixPackNodeProject/server/routes/users.routes.js
@@ -2,17 +2,22 @@
 import express from "express";
 import {
 	getAllUsers,
-	getUser,
-	createNewUser,
+	getUserProfile,
+	loginUser,
+	logoutUser,
+	registerUser,
 } from "../controller/users.controller.js";
+import { isAuthenticated } from "../middlewares/auth.js";
 
 // Creating Router
 const router = express.Router();
 
 // Routes
 router.get("/all", getAllUsers);
-router.get("/:id", getUser);
-router.post("/new", createNewUser);
+router.get("/me", isAuthenticated, getUserProfile);
+router.get("/logout", logoutUser);
+router.post("/new", registerUser);
+router.post("/login", loginUser);
 
 // exports
 export default router;
