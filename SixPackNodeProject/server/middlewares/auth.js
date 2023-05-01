93a35d502c9124d84b8379a8bf3844fe054ec56a
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-05-01 18:36:55 +0530

    auth logic middleware

---
+++ SixPackNodeProject/server/middlewares/auth.js
@@ -0,0 +1,22 @@
+import { User } from "../models/users.models.js";
+import jwt from "jsonwebtoken";
+
+export const isAuthenticated = async (req, res, next) => {
+	// getting user cookies
+	const { token } = req.cookies;
+
+	if (!token) {
+		return res.json({
+			success: false,
+			message: "Login first!!",
+		});
+	}
+
+	// Verifying the token
+	const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+	// attaching user to the request
+	const user = await User.findById(decoded._id);
+	req.user = user;
+	next();
+};
