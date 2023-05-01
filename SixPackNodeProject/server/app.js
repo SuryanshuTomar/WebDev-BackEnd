4996162caed46c64ebd04ebe2e73adf2ec262e4f
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-05-01 18:36:13 +0530

    cookie parser middleware and prefix path for users

--- SixPackNodeProject/server/app.js
+++ SixPackNodeProject/server/app.js
@@ -1,10 +1,12 @@
 // imports
 import express from "express";
 import userRouter from "./routes/users.routes.js";
+import cookieParser from "cookie-parser";
 
 // create express app
 export const app = express();
 
 // middlewares
 app.use(express.json());
-app.use("/users", userRouter);
+app.use(cookieParser());
+app.use("/api/v1/users", userRouter);
