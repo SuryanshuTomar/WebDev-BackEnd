576864a89023ff278256124774389421d015f7b2
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-05-01 18:35:53 +0530

    added sendCookie logic

---
+++ SixPackNodeProject/server/utils/features.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+
+export const sendCookie = (user, res, message, statusCode = 200) => {
+	// create token
+	const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+
+	res.status(statusCode)
+		.cookie("token", token, {
+			httpOnly: true,
+			maxAge: 1000 * 60 * 15,
+		})
+		.json({
+			success: true,
+			message,
+		});
+
+
+   };
+
+
