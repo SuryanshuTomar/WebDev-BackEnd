ffd479f0cbc4383acdbccd688a384b4b25176545
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-05-06 21:22:51 +0530

    using error Handler Middleware

--- SixPackNodeProject/server/app.js
+++ SixPackNodeProject/server/app.js
@@ -3,6 +3,7 @@ import express from "express";
 import userRouter from "./routes/users.route.js";
 import cookieParser from "cookie-parser";
 import taskRouter from "./routes/tasks.route.js";
+import { errorMiddleware } from "./middlewares/error.js";
 
 // create express app
 export const app = express();
@@ -12,3 +13,6 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tasks", taskRouter);
+
+// error handler
+app.use(errorMiddleware);
