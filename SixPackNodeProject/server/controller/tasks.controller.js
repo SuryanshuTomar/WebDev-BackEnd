0bf897e3bfdad9947aa550b93d52a8d9ee95ddf1
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-05-06 21:23:40 +0530

    added try catch block for async controllers

--- SixPackNodeProject/server/controller/tasks.controller.js
+++ SixPackNodeProject/server/controller/tasks.controller.js
@@ -1,59 +1,112 @@
+import mongoose from "mongoose";
 import { Task } from "../models/tasks.models.js";
+import { createCustomError } from "../utils/errorHandler.js";
 
 const createNewTask = async (req, res, next) => {
-	const { title, description, isCompleted } = req.body;
-	const { user } = req;
+	try {
+		const { title, description, isCompleted } = req.body;
+		const { user } = req;
 
-	console.log(req.body);
-	const task = await Task.create({
-		title,
-		description,
-		isCompleted,
-		user,
-	});
+		// create a new task
+		const task = await Task.create({
+			title,
+			description,
+			isCompleted,
+			user,
+		});
 
-	res.status(201).json({
-		success: true,
-		task,
-	});
+		// send response
+		res.status(201).json({
+			success: true,
+			task,
+		});
+	} catch (error) {
+		next(error);
+	}
 };
 
 const getAllTasks = async (req, res, next) => {
-	// get current user _id
-	const userId = req.user._id;
+	try {
+		// get current user _id
+		const userId = req.user._id;
 
-	// now get all task for the user who is logged in
-	const tasks = await Task.find({ user: userId });
+		// now get all task for the user who is logged in
+		const tasks = await Task.find({ user: userId });
 
-	res.status(200).json({
-		success: true,
-		tasks,
-	});
+		res.status(200).json({
+			success: true,
+			tasks,
+		});
+	} catch (error) {
+		next(error);
+	}
 };
 
 const getTask = async (req, res, next) => {};
 
 const updateTask = async (req, res, next) => {
-	const { id } = req.params;
+	try {
+		const { id } = req.params;
 
-	let task = await Task.findById(id);
-	task.isCompleted = !task.isCompleted;
-	task = await task.save();
+		// Check whether the it the object id provided is valid or not
+		// if (!mongoose.isValidObjectId(id)) {
+		// or
+		if (!mongoose.Types.ObjectId.isValid(id)) {
+			return next(new Error());
+		}
 
-	res.status(200).json({
-		success: true,
-		task,
-	});
+		// check whether the task for this id exists or not
+		let task = await Task.findById(id);
+
+		// if the task doesn't exists then throw error
+		if (!task) {
+			return next(createCustomError("Task doesn't exists!!", 404));
+		}
+
+		// if the task exists then update the task
+		task.isCompleted = !task.isCompleted;
+		task = await task.save();
+
+		//	send response with the updated task
+		res.status(200).json({
+			success: true,
+			task,
+		});
+	} catch (error) {
+		next(error);
+	}
 };
 
 const deleteTask = async (req, res, next) => {
-	const { id } = req.params;
-	let task = await Task.findByIdAndDelete(id);
+	try {
+		const { id } = req.params;
 
-	res.status(200).json({
-		success: true,
-		task,
-	});
+		// Check whether the it the object id provided is valid or not
+		// if (!mongoose.isValidObjectId(id)) {
+		// or
+		if (!mongoose.Types.ObjectId.isValid(id)) {
+			return next(createCustomError("Invalid Id", 404));
+		}
+
+		// check whether the task for this id exists or not
+		let task = await Task.findById(id);
+
+		// if the task doesn't exists then throw error
+		if (!task) {
+			return next(createCustomError("Task doesn't exists!!", 404));
+		}
+
+		// if the task exists then delete the task
+		await task.deleteOne();
+
+		//	send response with the deleted task
+		res.status(200).json({
+			success: true,
+			task,
+		});
+	} catch (error) {
+		next(error);
+	}
 };
 
 export { createNewTask, getAllTasks, getTask, updateTask, deleteTask };
