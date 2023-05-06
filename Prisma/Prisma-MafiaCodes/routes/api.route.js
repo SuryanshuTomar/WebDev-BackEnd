718a01e69a4381dc4df3862ecbffe4221336e97a
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-05-06 12:03:37 +0530

    added products routes

--- Prisma/Prisma-MafiaCodes/routes/api.route.js
+++ Prisma/Prisma-MafiaCodes/routes/api.route.js
@@ -1,7 +1,23 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/', async (req, res, next) => {
-  res.send({ message: 'Ok api is working 🚀' });
+router.get("/", async (req, res, next) => {
+	res.send({ message: "Ok api is working 🚀" });
+});
+
+router.get("/:id", async (req, res, next) => {
+	res.send({ message: "Ok api is working 🚀" });
+});
+
+router.post("/", async (req, res, next) => {
+	res.send({ message: "Ok api is working 🚀" });
+});
+
+router.delete("/:id", async (req, res, next) => {
+	res.send({ message: "Ok api is working 🚀" });
+});
+
+router.patch("/:id", async (req, res, next) => {
+	res.send({ message: "Ok api is working 🚀" });
 });
 
 module.exports = router;
