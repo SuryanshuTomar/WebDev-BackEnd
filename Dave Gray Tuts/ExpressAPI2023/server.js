0a3d86b3b94b1535a4d005caa11ca4b4060545ef
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-05-07 16:39:53 +0530

    uncomment code

--- Dave Gray Tuts/ExpressAPI2023/server.js
+++ Dave Gray Tuts/ExpressAPI2023/server.js
@@ -38,24 +38,24 @@ app.use(logger);
 // -------------------------------------------------------------------------------------
 // App middlewares
 const whitelist = ["http://localhost:8000", "http://127.0.0.1:8000"];
-// const corsOptions = {
-// 	origin: (origin, cbFn) => {
-// 		// if the origin is localhost then it will be undefined in that case and
-// 		// if the origin which requested the server is present in the whitelist then OK.
-// 		if (!origin ?? whitelist.includes(origin)) {
-// 			cbFn(null, true);
-// 		}
-// 		// else throw a new error
-// 		else {
-// 			cbFn(new Error("Not allowed by CORS"));
-// 		}
-// 	},
-// 	optionsSuccessStatus: 200,
-// 	method: ["GET", "POST", "PUT", "DELETE"],
-// 	credentials: true,
-// };
+const corsOptions = {
+	origin: (origin, cbFn) => {
+		// if the origin is localhost then it will be undefined in that case and
+		// if the origin which requested the server is present in the whitelist then OK.
+		if (!origin ?? whitelist.includes(origin)) {
+			cbFn(null, true);
+		}
+		// else throw a new error
+		else {
+			cbFn(new Error("Not allowed by CORS"));
+		}
+	},
+	optionsSuccessStatus: 200,
+	method: ["GET", "POST", "PUT", "DELETE"],
+	credentials: true,
+};
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser());
