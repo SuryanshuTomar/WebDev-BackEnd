576a55560668b21aa013165f3a1435e66511474e
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-05-07 15:53:17 +0530

    setup root and subdir router

--- Dave Gray Tuts/ExpressAPI2023/server.js
+++ Dave Gray Tuts/ExpressAPI2023/server.js
@@ -7,6 +7,8 @@ const cookieParser = require("cookie-parser");
 require("dotenv").config({ path: "./config.env" });
 
 const { logger } = require("./middlewares/logEvents");
+const { subdirRouter } = require("./routes/subdir.route");
+const { rootRouter } = require("./routes/root.route");
 const errorHandler = require("./middlewares/errorHandler");
 
 // -------------------------------------------------------------------------------------
@@ -18,7 +20,12 @@ const app = express();
 // Serving static files from server
 // app.use(express.static(path.join(path.resolve(), "css")));
 // app.use(express.static(path.join(__dirname, "img")));
+
+// use public folder for both views and subdir in views
+// app.use("/", express.static(path.join(path.resolve(), "public")));
+// or
 app.use(express.static(path.join(path.resolve(), "public")));
+app.use("/subdir", express.static(path.join(path.resolve(), "public")));
 
 // Setting view engine
 app.set("view engine", "ejs");
@@ -54,21 +61,9 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 
 // -------------------------------------------------------------------------------------
-// Route Handlers
+// Route Handler
 app.get("/$|index(.html)?", (req, res) => {
 	res.render("index", { name: "Alex" });
-});
-
-app.get("/new-page(.html)?", (req, res) => {
-	res.render("new-page");
-});
-
-app.get("/subdir", (req, res) => {
-	res.render("subdir/index");
-});
-
-app.get("/test", (req, res) => {
-	res.render("subdir/test");
 });
 
 // -------------------------------------------------------------------------------------
@@ -85,21 +80,22 @@ const two = (req, res, next) => {
 
 const three = (req, res, next) => {
 	console.log("three");
-	next();
+	res.send("Recieved Chain!");
 };
 
 app.get("/chain(.html)?", [one, two, three]);
 
 // -------------------------------------------------------------------------------------
+// API Route middlewares
+app.use("/", rootRouter);
+app.use("/subdir", subdirRouter);
+
+// -------------------------------------------------------------------------------------
 // API Route Not-Found handler
-// app.use(), doesn't accepts regex for paths.
 // app.all(), accepts all http methods and regex for path as well.
 app.all("*", (req, res) => {
 	res.status(404).render("404");
 });
-
-// -------------------------------------------------------------------------------------
-// API Route middlewares
 
 // -------------------------------------------------------------------------------------
 // Custom Middleware - Error Handler
