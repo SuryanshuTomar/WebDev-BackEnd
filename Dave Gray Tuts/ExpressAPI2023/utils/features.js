8e9191f9d2156de31311a032439ee883f7242df4
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-05-12 11:41:36 +0530

    features notes

---
+++ Dave Gray Tuts/ExpressAPI2023/utils/features.js
@@ -0,0 +1,6 @@
+// Here we can create a function for -
+// 1. create a new token
+// 2. Send the cookie along with response whenever the user registers or log in
+// 3. verify the token (not the handler just token verification)
+// 4. async-wrapper
+// 5. Custom Error class
