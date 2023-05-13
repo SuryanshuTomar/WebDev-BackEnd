3ab57dff3793250d20f07ea01ebb28e85b055807
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-05-13 21:45:31 +0530

    added some info on cookies

--- SixPackNodeProject/server/utils/features.js
+++ SixPackNodeProject/server/utils/features.js
@@ -8,8 +8,8 @@ export const sendCookie = (user, res, message, statusCode = 200) => {
 		.cookie("token", token, {
 			httpOnly: true,
 			maxAge: 1000 * 60 * 15, // token cookie will expire in 15mins.
-			sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none", // This means we can now share the cookie in cross origin.
-			secure: process.env.NODE_ENV === "Development" ? false : true, // if "sameSite" is set to "none" then "secure" must be set to "true".
+			sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none", // Setting this to "none" means we can now share the cookie in cross origin. By default it is set to "lax" in which we can only share in same origin.
+			secure: process.env.NODE_ENV === "Development" ? false : true, // if "sameSite" is set to "none" then "secure" must be set to "true". // Note: we have to set the property "secure: true" also along with the httpOnly property and this "secure" property will allow us to send the cookie along with response only to the https server.
 		})
 		.json({
 			success: true,
