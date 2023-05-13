da4ae33e6490a7297979cf9e6241b6fce045a22e
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-05-13 18:06:10 +0530

    added refreshtoken route to refresh access token

--- Dave Gray Tuts/ExpressAPI2023/routes/api/users.route.js
+++ Dave Gray Tuts/ExpressAPI2023/routes/api/users.route.js
@@ -4,12 +4,12 @@ const router = express.Router();
 const {
 	registerUser,
 	loginUser,
+	handleRefreshToken,
 } = require("../../controllers/users.controller");
 
 // user routes
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-
-
+router.get("/refreshtoken", handleRefreshToken);
 
 module.exports = router;
